d25bd2ee495a8f9eec25fb45adcf8b52b6e05f98
Kenil Sheliya <63154163+Kenil-1057@users.noreply.github.com>
2024-06-08 10:54:32 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "*"
+  ],
+  theme: {
+    screens: {
+      xsm: '480px',
+      sm: '575px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        xsm:'480px',
+        sm: '575px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
+    },
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
